f2cfc2d78d263bffd99721afcdd0750cd0678057
esullivan <esullivan@163.com>
2024-07-25 16:25:54 +0800

    feat: element plu、自动导入icon整合

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
+
 const pathSrc: string = path.resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
@@ -17,12 +21,21 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       },
     },
     server: {
-      // proxy: "0.0.0.0",
+      host: "0.0.0.0",
       port: 3000,
+      open: true,
     },
     plugins: [
       vue(),
       AutoImport({
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        // 是否在 vue 模板中自动导入
+        vueTemplate: true,
         // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
         imports: ["vue"],
         eslintrc: {
@@ -30,11 +43,26 @@ export default ({ mode }: ConfigEnv): UserConfig => {
           filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
         },
         dts: false,
-        // dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 指定自动导入函数TS类型声明文件路径
+        // 指定自动导入函数TS类型声明文件路径
+        // dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"),
       }),
       Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+            enabledCollections: ["ep"],
+          }),
+        ],
         dts: false,
-        // dts: path.resolve(pathSrc, "types", "components.d.ts"), // 指定自动导入组件TS类型声明文件路径
+        // 指定自动导入组件TS类型声明文件路径
+        // dts: path.resolve(pathSrc, "types", "components.d.ts"),
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
       }),
     ],
   };
